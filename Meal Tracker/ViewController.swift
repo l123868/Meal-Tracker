a2646b9a2b10ee0e16091638fe27b445e745c967
Latifah <l123868@gmail.com>
2022-04-15 10:30:48 +0800

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Latifah on 2022/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
