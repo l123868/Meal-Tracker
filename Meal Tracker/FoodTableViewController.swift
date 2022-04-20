659ff11bb773d5898355404737559474dd6e135a
Latifah <l123868@gmail.com>
2022-04-20 21:12:54 +0800

    change: some typo error

--- Meal Tracker/FoodTableViewController.swift
+++ Meal Tracker/FoodTableViewController.swift
@@ -11,7 +11,7 @@ class FoodTableViewController: UITableViewController {
 
     var meals : [Meal]{
         [Meal(name: "breakfast", food: [Food(name: "蛋餅", description: "有澱粉"), Food(name: "漢堡", description: "有夾生菜"), Food(name: "中式套餐", description: "蘿蔔糕、豬排、豆漿")]),
-         Meal(name: "lunch", food: [Food(name: "自助餐", description: "可以自己夾"), Food(name: "便當", description: "和公司管理部定"), Food(name: "肯德基", description: "和同事一起吃的邪惡食物")]),
+         Meal(name: "lunch", food: [Food(name: "自助餐", description: "可以自己夾"), Food(name: "便當", description: "和公司管理部訂"), Food(name: "肯德基", description: "和同事一起吃的邪惡食物")]),
          Meal(name: "dinner", food: [Food(name: "便當", description: "跟中午一起買"), Food(name: "飲料", description: "果汁最棒了"), Food(name: "湯", description: "喝湯是一定要的！")])]
     }
     
